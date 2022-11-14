da15946d1b0dad059505faf2f766cba6b4d11d13
shahriGa <97836229+shahriGa@users.noreply.github.com>
2022-11-14 19:52:52 +0500

    am

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// var a = 10;
+// var b = 5;
+// console.log(a + b);
+
+// var a = 25;
+// var b = 15;
+// var result = a - b;
+// console.log(result);
+
+// var a = 10;
+// var b = 5;
+// var c = 10 - 5;
+// console.log(c);
+
+// var a = 10;
+// var b = 3;
+// console.log(a / b);
+
+alert("saytimizga xush kelipsiz");
+
+var name = prompt("ismingiz:");
+var age = prompt("yoshingiz:");
+var univer = prompt("okish joyingiz");
+var dream = prompt("kelajakdaki orzuyingiz:");
+
+console.log(
+  `assalom aleykum mening ismim ${name} yoshim ${age}da xozirda ${univer}da okiyman kelajakdagi orzuyim ${dream} bolish`
+);
